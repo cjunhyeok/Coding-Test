035d864552abf7a42ef6a37298908ab59eb212e4
chlwnsgur <chlwnsgur1214@naver.com>
2026-01-31 17:58:59 +0900

    백준 미로 탐색(2178) 풀이 실패 - bfs 최단거리 알고리즘 복습 필요

---
+++ src/main/java/algorithm/dfsbfs/미로탐색_2178_백준.java
@@ -0,0 +1,64 @@
+package main.java.algorithm.dfsbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 미로탐색_2178_백준 {
+
+    static int[][] map;
+    static int N, M;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(line, " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+
+        System.out.println(map[N - 1][M - 1]);
+    }
+
+    static void bfs(int startX, int startY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{startX, startY});
+
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+                    continue;
+                }
+
+                if (map[nextX][nextY] != 1) {
+                    continue;
+                }
+
+                map[nextX][nextY] = map[x][y] + 1;
+                queue.add(new int[]{nextX, nextY});
+            }
+        }
+    }
+}
