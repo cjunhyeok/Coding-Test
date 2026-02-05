6e95d294178756903e10f94e386009cb89dd11be
chlwnsgur <chlwnsgur1214@naver.com>
2026-02-05 21:41:55 +0900

    백준 신입 사원(1946) 풀이 실패 - 처음 문제 이해 실패 - 타 풀이 중 첫번째 이후 반복에서 힌트를 얻어서 풀이 성공

---
+++ src/main/java/algorithm/greedy/신입사원_1946_백준.java
@@ -0,0 +1,51 @@
+package main.java.algorithm.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 신입사원_1946_백준 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T --> 0) {
+            int N = Integer.parseInt(br.readLine());
+            List<int[]> lists = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                int[] value = new int[]{a, b};
+                lists.add(value);
+            }
+
+            Collections.sort(lists, (s1, s2) -> {
+                return s1[0] - s2[0];
+            });
+
+            int result = lists.size();
+            int maxValue = lists.get(0)[1];
+
+            for (int i = 1; i < lists.size(); i++) {
+                int value = lists.get(i)[1];
+
+                if (value > maxValue) {
+                    result--;
+                } else if (value < maxValue) {
+                    maxValue = value;
+                }
+            }
+
+            System.out.println(result);
+        }
+    }
+}
