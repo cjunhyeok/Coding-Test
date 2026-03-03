3e80fc86c6ebbadde3aa3151215ba92e4fec005f
chlwnsgur <chlwnsgur1214@naver.com>
2026-03-03 16:12:49 +0900

    2xn 타일링 (11726) 풀이 실패 - 점확식은 맞음 - % 계산을 출력할 때 하는게 아닌 계산할 때 수행해줘야함

---
+++ src/main/java/algorithm/dp/타일링2XN_11726_백준.java
@@ -0,0 +1,24 @@
+package main.java.algorithm.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 타일링2XN_11726_백준 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
