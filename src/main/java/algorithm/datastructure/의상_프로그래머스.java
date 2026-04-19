2fc7fe149167df4d3d4ea88bcb71e906ca3eb005
chlwnsgur <chlwnsgur1214@naver.com>
2026-04-19 20:28:11 +0900

    프로그래머스 의상 풀이 실패 - 간단한 조합 계산법을 떠올리지 못 함

---
+++ src/main/java/algorithm/datastructure/의상_프로그래머스.java
@@ -0,0 +1,30 @@
+package main.java.algorithm.datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상_프로그래머스 {
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            String kind = clothes[i][1];
+
+            if (map.containsKey(kind)) {
+                Integer value = map.get(kind);
+                map.put(kind, value + 1);
+            } else {
+                map.put(kind, 1);
+            }
+        }
+
+        for (Integer value : map.values()) {
+            answer = answer * (value + 1);
+        }
+
+        return answer - 1;
+    }
+}
