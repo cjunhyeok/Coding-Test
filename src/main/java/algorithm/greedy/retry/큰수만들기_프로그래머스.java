b152c74c0d4dbdac72f65ada353ac5cc0ace59ce
chlwnsgur <chlwnsgur1214@naver.com>
2026-07-02 21:10:38 +0900

    프로그래머스 큰 수 만들기 재시도 성공

--- src/main/java/algorithm/greedy/retry/큰수만들기_프로그래머스.java
+++ src/main/java/algorithm/greedy/retry/큰수만들기_프로그래머스.java
@@ -3,20 +3,20 @@ package main.java.algorithm.greedy.retry;
 import java.util.Stack;
 
 public class 큰수만들기_프로그래머스 {
-
-    public static String solution(String number, int k) {
-
+    public String solution(String number, int k) {
         Stack<Integer> stack = new Stack<>();
+
         for (int i = 0; i < number.length(); i++) {
+
             int value = number.charAt(i) - '0';
 
-            if (stack.isEmpty()) {
-                stack.add(value);
-                continue;
-            }
+            while (!stack.isEmpty()) {
+                if (k == 0) {
+                    break;
+                }
 
-            while (!stack.isEmpty() && k > 0) {
-                if (stack.peek() < value) {
+                Integer peek = stack.peek();
+                if (peek < value) {
                     stack.pop();
                     k--;
                 } else {
