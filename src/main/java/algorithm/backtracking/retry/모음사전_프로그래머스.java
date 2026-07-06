7e4404122269b9af57f9ccd96dd11b2d6ea7f109
chlwnsgur <chlwnsgur1214@naver.com>
2026-07-06 11:44:14 +0900

    프로그래머스 모음사전 재시도 성공

--- src/main/java/algorithm/backtracking/retry/모음사전_프로그래머스.java
+++ src/main/java/algorithm/backtracking/retry/모음사전_프로그래머스.java
@@ -2,32 +2,31 @@ package main.java.algorithm.backtracking.retry;
 
 public class 모음사전_프로그래머스 {
 
+    private static String[] values = new String[]{"A", "E", "I", "O", "U"};
     private static int count = 0;
     private static int answer = 0;
-    private static String[] values = new String[]{"A", "E", "I", "O", "U"};
 
     public int solution(String word) {
+        answer = 0;
+
         dfs("", word);
+
         return answer;
     }
 
-    private static void dfs(String start, String target) {
-        if (start.equals(target)) {
+    private static void dfs(String current, String target) {
+        if (current.equals(target)) {
             answer = count;
             return;
         }
 
-        if (start.length() == 5) {
+        if (current.length() == 5) {
             return;
         }
 
         for (String value : values) {
             count++;
-            dfs(start + value, target);
-
-            if (answer != 0) {
-                return;
-            }
+            dfs(current + value, target);
         }
     }
 }
