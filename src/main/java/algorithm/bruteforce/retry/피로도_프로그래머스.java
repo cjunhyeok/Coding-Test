82cd3830481f0d27950db7f451ba0fd1d5d1cba6
chlwnsgur <chlwnsgur1214@naver.com>
2026-08-02 14:58:39 +0900

    프로그래머스 피로도 재시도 성공

--- src/main/java/algorithm/bruteforce/retry/피로도_프로그래머스.java
+++ src/main/java/algorithm/bruteforce/retry/피로도_프로그래머스.java
@@ -1,56 +1,42 @@
 package main.java.algorithm.bruteforce.retry;
 
 public class 피로도_프로그래머스 {
-
     private static int N;
     private static boolean[] used;
-    private static int answer = -1;
+    private static int answer = 0;
 
     public int solution(int k, int[][] dungeons) {
-
         N = dungeons.length;
         used = new boolean[N];
 
         for (int i = 0; i < N; i++) {
-            int[] dungeon = dungeons[i];
-            int min = dungeon[0];
-            int use = dungeon[1];
-
-            if (k < min) {
-                continue;
-            }
-
-            used[i] = true;
-            dfs(1, k - use, dungeons);
-            used[i] = false;
+            dfs(i, k, 0, dungeons);
         }
 
         return answer;
     }
 
-    private static void dfs(int depth, int k, int[][] dungeons) {
-
-        answer = Math.max(answer, depth);
+    private static void dfs(int depth, int k, int count, int[][] dungeons) {
         if (depth == N) {
+            answer = Math.max(answer, count);
             return;
         }
-
+        
         for (int i = 0; i < N; i++) {
+
+            int minFatigue = dungeons[i][0];
+            int consumeFatigue = dungeons[i][1];
+
+            if (k < minFatigue) {
+                continue;
+            }
 
             if (used[i]) {
                 continue;
             }
 
-            int[] dungeon = dungeons[i];
-            int min = dungeon[0];
-            int use = dungeon[1];
-
-            if (k < min) {
-                continue;
-            }
-
             used[i] = true;
-            dfs(depth + 1, k - use, dungeons);
+            dfs(depth + 1, k - consumeFatigue, count + 1, dungeons);
             used[i] = false;
         }
     }
