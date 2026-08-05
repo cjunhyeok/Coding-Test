2224192bb13a4479fcec2e826a27017b67f68f65
chlwnsgur <chlwnsgur1214@naver.com>
2026-08-05 13:27:27 +0900

    프로그래머스 정수 삼각형 재시도 성공

--- src/main/java/algorithm/dp/retry/정수삼각형_프로그래머스.java
+++ src/main/java/algorithm/dp/retry/정수삼각형_프로그래머스.java
@@ -1,24 +1,21 @@
 package main.java.algorithm.dp.retry;
 
 public class 정수삼각형_프로그래머스 {
-    public int solution(int[][] triangle) {
+    public static int solution(int[][] triangle) {
         int answer = 0;
 
-        int N = triangle.length;
-        int[][] dp = new int[N][N];
-
+        int[][] dp = new int[triangle.length][triangle.length];
         dp[0][0] = triangle[0][0];
 
-        for (int i = 1; i < N; i++) {
-            int[] values = triangle[i];
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                int value = triangle[i][j];
 
-            for (int j = 0 ; j < values.length; j++) {
-                if (j > 0) {
-                    dp[i][j] = Math.max(dp[i][j], triangle[i][j] + dp[i - 1][j - 1]);
-                    dp[i][j] = Math.max(dp[i][j], triangle[i][j] + dp[i - 1][j]);
-                } else {
-                    dp[i][j] = Math.max(dp[i][j], triangle[i][j] + dp[i - 1][j]);
+                if (j - 1 >= 0) {
+                    dp[i][j] = dp[i - 1][j - 1] + value;
                 }
+
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + value);
 
                 answer = Math.max(answer, dp[i][j]);
             }
