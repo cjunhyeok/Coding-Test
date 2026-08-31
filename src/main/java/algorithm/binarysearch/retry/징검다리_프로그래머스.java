f4b8fa0d5ce4c06a7620c6f6f9d992df5a3e809a
chlwnsgur <chlwnsgur1214@naver.com>
2026-08-31 13:41:51 +0900

    프로그래머스 징검다리 재시도 실패

--- src/main/java/algorithm/binarysearch/retry/징검다리_프로그래머스.java
+++ src/main/java/algorithm/binarysearch/retry/징검다리_프로그래머스.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class 징검다리_프로그래머스 {
     public int solution(int distance, int[] rocks, int n) {
+        long answer = 0;
+
         Arrays.sort(rocks);
 
-        long left = 1;
+        long left = 0;
         long right = distance;
-        long answer = 0;
 
         while (left <= right) {
             long mid = (left + right) / 2;
@@ -19,21 +20,24 @@ public class 징검다리_프로그래머스 {
             for (int rock : rocks) {
                 if (rock - current < mid) {
                     removed++;
+
+                    if (removed > n) {
+                        break;
+                    }
                 } else {
                     current = rock;
                 }
             }
 
-            // 마지막 남은 돌 ~ 도착점 거리도 확인
             if (distance - current < mid) {
                 removed++;
             }
 
             if (removed <= n) {
                 answer = mid;
-                left = mid + 1;   // 더 큰 최소거리도 가능한지 확인
+                left = mid + 1;
             } else {
-                right = mid - 1;  // 제거가 너무 많이 필요하므로 거리 줄이기
+                right = mid - 1;
             }
         }
 
